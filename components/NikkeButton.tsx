7e36c15db4cdd1967c1d3f9ba2671868d938ba57
LeeVancee <1280403599@qq.com>
2024-05-22 01:01:27 +0800

    fix font

--- components/NikkeButton.tsx
+++ components/NikkeButton.tsx
@@ -7,30 +7,17 @@ interface NikkeButtonProps {
   content: string | undefined;
   onClick?: () => void; // 你可以根据实际需要调整
   style?: React.CSSProperties; // 添加 style 属性
+  contents?: string;
 }
 
-const NikkeButton = ({ type, content, style, onClick }: NikkeButtonProps) => {
+const NikkeButton = ({ type, content, style, onClick, contents }: NikkeButtonProps) => {
   return (
     <div className={styles.btn} style={style} onClick={onClick}>
-      <span
-        className={`${styles.value} ${
-          type === buttonType.Cancel ? 'valueExit' : ''
-        }`}
-      >
+      <span className={`${styles.value} ${type === buttonType.Cancel ? 'valueExit' : ''}`}>
         {content === undefined ? '按钮' : content}
       </span>
-      {type === buttonType.Success && (
-        <Image src="/btn_bg.png" width={150} height={50} alt="" />
-      )}
-      {type === buttonType.Cancel && (
-        <Image
-          src="/btn_white_bg.png"
-          width={150}
-          height={50}
-          alt=""
-          priority
-        />
-      )}
+      {type === buttonType.Success && <Image src="/btn_bg.png" width={150} height={50} alt="" />}
+      {type === buttonType.Cancel && <Image src="/btn_white_bg.png" width={150} height={50} alt="" priority />}
     </div>
   );
 };
--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
